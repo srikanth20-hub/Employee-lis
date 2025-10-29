8e95b24633c9ca2fc4aa77bab1c3e053d17acbe3
srikanth <kummarisrikanth626@gmail.com>
2025-10-29 09:43:01 +0530

    Emp app intialized

---
+++ employees/views.py
@@ -0,0 +1,13 @@
+from django.http import  HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from employees.models import Employee
+
+# Create your views here.
+
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee' : employee,
+    }
+    return render(request, 'employee_detail.html', context)
